ab0656a22bf53b65e775525a4ccf213696015fa6
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-05-10 10:42:12 +0200

    feat: update linkedin tag

--- src/linkedin.ts
+++ src/linkedin.ts
@@ -3,15 +3,20 @@ import { TrackingId, useTrackingMeta } from "./shared";
 export function useLinkedinInsightTag(maybeId: TrackingId) {
   useTrackingMeta(maybeId, (id) => ({
     script: {
-      linkedinInsightTag: {
+      linkedinPartnerId: {
         type: "text/javascript",
         innerHTML: `window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-        window._linkedin_data_partner_ids.push(${id});
-        (function(){var s = document.getElementsByTagName("script")[0];
+        window._linkedin_data_partner_ids.push(${id});`,
+      },
+      linkedinInsightTag: {
+        type: "text/javascript",
+        innerHTML: `(function(l) {if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+        window.lintrk.q=[]}
+        var s = document.getElementsByTagName("script")[0];
         var b = document.createElement("script");
         b.type = "text/javascript";b.async = true;
         b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-        s.parentNode.insertBefore(b, s);})();`,
+        s.parentNode.insertBefore(b, s);})(window.lintrk);`,
       },
     },
     noscript: {
